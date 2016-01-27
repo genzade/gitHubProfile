c9c5bd3f6d7fcc682456478e9bea27fdc0d3d30b
Hamid Zade <hamidmamadzade@gmail.com>
2016-01-27 19:27:35 +0000

    GitUserSearchController added

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -2,22 +2,22 @@ module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '../',
+    basePath: "../",
 
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: ["jasmine"],
 
 
     // list of files / patterns to load in the browser
     files: [
-    'bower_components/angular/angular.js',
-    'bower_components/angular-route/angular-route.js',
-    'bower_components/angular-resource/angular-resource.js',
-    'bower_components/angular-mocks/angular-mocks.js',
-    'js/**/*.js',
-    'test/**/*.spec.js'
+    "bower_components/angular/angular.js",
+    "bower_components/angular-route/angular-route.js",
+    "bower_components/angular-resource/angular-resource.js",
+    "bower_components/angular-mocks/angular-mocks.js",
+    "js/**/*.js",
+    "test/**/*.spec.js"
     ],
 
 
@@ -31,9 +31,9 @@ module.exports = function(config) {
 
 
     // test results reporter to use
-    // possible values: 'dots', 'progress'
+    // possible values: "dots", "progress"
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    reporters: ["progress"],
 
 
     // web server port
@@ -55,7 +55,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['PhantomJS'],
+    browsers: ["PhantomJS"],
 
 
     // Continuous Integration mode
