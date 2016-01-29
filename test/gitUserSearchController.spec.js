3808a8713d2db3471efee808c0530afebcc67930
Hamid Zade <hamidmamadzade@gmail.com>
2016-01-29 12:48:57 +0000

    mocks in controller tests applied

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -3,37 +3,28 @@ describe("GitUserSearchController", function() {
 
   var ctrl;
 
-  beforeEach(inject(function($controller) {
-    ctrl = $controller("GitUserSearchController");
-  }));
-
-  it("initialises with an empty search result and term", function() {
-    expect(ctrl.searchResult).toBeUndefined();
-    expect(ctrl.searchTerm).toBeUndefined();
-  });
-
   describe("when searching for a user", function() {
-    var httpBackend;
+    var fakeUserInfo, scope, fakeSearch;
 
-    beforeEach(inject(function($httpBackend) {
-      httpBackend = $httpBackend;
-      httpBackend
-        .expectGET("https://api.github.com/search/users?access_token=" + access_token + "&q=tansaku")
-        .respond(
-          { items: items }
-        );
-    }));
+    beforeEach(function() {
+      fakeSearch = jasmine.createSpyObj("fakeSearch", ["query"]);
 
-    afterEach(function() {
-      httpBackend.verifyNoOutstandingExpectation();
-      httpBackend.verifyNoOutstandingRequest();
+      module({
+        Search: fakeSearch
+      });
     });
+    
+    beforeEach(inject(function($q, $rootScope, $controller) {
+      scope = $rootScope;
+      fakeSearch.query.and.returnValue($q.when( { data: items }));
+      ctrl = $controller("GitUserSearchController");
+    }));
 
     it("displays search results", function() {
       ctrl.searchTerm = "tansaku";
       ctrl.doSearch();
-      httpBackend.flush();
-      expect(ctrl.searchResult.items).toEqual(items);
+      scope.$apply();
+      expect(ctrl.searchResult).toEqual(items);
     });
   });
 });
